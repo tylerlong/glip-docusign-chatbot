298ea0ba7d29065da4b3659c9b51d0b04414ece6
Tyler Long <tyler4long@gmail.com>
2020-05-14 17:55:32 -0700

    Going to test it

--- README.md
+++ README.md
@@ -1,6 +1,32 @@
 # Glip DocuSign Chatbot
 
 
+## Usage
+
+```
+curl -X POST \
+-H "Content-Type: application/json" \
+-d '{"docusign":{"link":"http://docusign.com/xxx"},"glip":{"email":"first.last@ringcentral.com"}}' \
+https://xxxx.amazonaws.com/prod/webhook
+```
+
+## Payload structure
+
+```json
+{
+    "docusign": {
+        "link": "http://docusign.com/xxx" // link to sign the DocuSign doc
+    },
+    "glip": {
+        "email":"first.last@ringcentral.com", // optional
+        "id": "123456" // optional
+    }
+}
+```
+
+Both `glip.email` and `glip.id` are optional, but you should at least specify one of them.
+
+
 ## For maintainers
 
 Please refer to [this](https://github.com/tylerlong/glip-ping-chatbot/tree/lambda).
--- lambda.js
+++ lambda.js
@@ -2,6 +2,7 @@ const createApp = require('ringcentral-chatbot/dist/apps').default
 const { createAsyncProxy } = require('ringcentral-chatbot/dist/lambda')
 const serverlessHTTP = require('serverless-http')
 const axios = require('axios')
+const { Bot } = require('ringcentral-chatbot/dist/models')
 
 const app = createApp()
 module.exports.app = serverlessHTTP(app)
@@ -14,9 +15,24 @@ module.exports.maintain = async () => axios.put(`${process.env.RINGCENTRAL_CHATB
 })
 
 module.exports.webhook = async (event) => {
+  const bots = await Bot.findAll()
+  if (!bots || bots.length === 0) {
+    return {
+      statusCode: 404,
+      body: 'Cannot find chatbot. Please ask admin to add chatbot to Glip.'
+    }
+  }
+  const bot = bots[0]
+  const json = JSON.parse(event.body)
+  let r = await bot.rc.post('/restapi/v1.0/glip/conversations', { members: [json.glip] })
+  console.log(JSON.stringify(r.data))
+  r = await await bot.rc.post('/restapi/v1.0/glip/posts', {
+    groupId: r.data.id,
+    text: `Please [sign this DocuSign Document](${json.docusign.link})`
+  })
+  console.log(JSON.stringify(r.data))
   return {
     statusCode: 200,
-    body: JSON.stringify(event),
-    headers: { 'Content-Type': 'application/json' }
+    body: 'Done'
   }
 }
